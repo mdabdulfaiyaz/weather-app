b86fe653748d47559c3a63420ea6b5ba1976a2ab
Faiyaz <md_faiyaz123@yahoo.com>
2021-06-19 15:54:11 +0530

    tmp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React from 'react'; 
 import './App.css';
+import { Container } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles" ;
+import Weathercard from './Weathercard';
+import Header from './Header';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight:'100vh',
+    maxWidth:'100vw', 
+    margin:'0px',
+    backgroundColor: '#353534',
+    boxShadow: '2px 0px 67px 4px rgba(0,0,0,0.49) inset',
+    paddingLeft:'0px',
+    paddingRight:'0px',
+    overflow:'hidden',
+  },
+}))
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const classes = useStyles()
+  return ( 
+  <Container className={classes.root}>
+    <Header /> 
+    <Weathercard />
+  </Container> 
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles((theme) => ({ 
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    padding: '0 15rem',
+    cursor: 'pointer',
+    color: "#262323",
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  appbar:{
+    background: 'transparent',
+    opacity: 0.5,
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.black, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.black, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+export default function SearchAppBar() {
+  const classes = useStyles();
+
+  const [checked, setchecked] = useState(false) 
+
+  useEffect(() => {
+    setchecked(true);
+  }, [])
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.appbar}>
+        <Toolbar> 
+          <Typography className={classes.title} variant="h6" noWrap>
+            Weather <span>Forecast</span>
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
